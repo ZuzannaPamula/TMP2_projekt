2eb3db21878c14e091ac9baafbf3b17bb3ade24e
Zuzanna Pamula <pamulazuzanna@gmail.com>
2022-06-22 20:34:57 +0200

    Podawanie sygnału o długości a i wprowadzenie stanu zerowego na czas t_przerwy
    Naprzemienne włączanie i wyłączanie stanu wysokiego na P2.0

--- main.c
+++ main.c
@@ -3,12 +3,14 @@
 #include <lab2_board.h>
 #include <stdio.h>
 
-unsigned int a = 100;
-unsigned int b = 200;
-unsigned int c = 300;
+unsigned int a = 5;
+unsigned int b = 10;
+unsigned int c = 30;
 unsigned int L = 0;
 unsigned int X = 0;
+unsigned int t_przerwy = 20;
 char nr_portu = 0;
+
 
 int main ()
 {		
@@ -18,19 +20,21 @@ int main ()
 		TF0 = 0;
 		X = a * 131;
 		L = 65536 - 1 - X;
-		P2&= ~(1<<nr_portu);
+		P2|= (1<<nr_portu);
 		TL0 = L%256;
 		TH0 = L/256;
 		while(TF0 == 0){
 			};
 		TF0 = 0;
-		P2|= (1<<nr_portu);
-		X = a * 131;
+		P2&= ~(1<<nr_portu);
+	
+		TF0 = 0;
+		X = t_przerwy * 131;
 		L = 65536 - 1 - X;
 		TL0 = L%256;
 		TH0 = L/256;
 		while(TF0 == 0){
 			};
-		TF0 = 0;	
+		TF0 = 0;
 		}
 }
